73b9d3d5abc1963d720db99135fdfa654079c171
LkrOverlord <menesestapia@gmail.com>
2023-09-02 15:20:55 -0300

    Se carga la ruta de home y limpia el html de component.html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  // ... tus otras rutas
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
