8f0fe2db4b9166f83ce0e454e67bded0b454d6e3
LkrOverlord <menesestapia@gmail.com>
2023-09-03 22:48:25 -0300

    Añadí componentes de footer, nav y carousel

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DiscountPipe } from './pipes/discount.pipe';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
 
 @NgModule({
   declarations: [
-    DiscountPipe
+    DiscountPipe,
+    NavComponent,
+    FooterComponent,
+    CarouselComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    DiscountPipe
+    DiscountPipe,
+    NavComponent,
+    FooterComponent,
+    CarouselComponent
   ]
 })
 export class SharedModule { }
