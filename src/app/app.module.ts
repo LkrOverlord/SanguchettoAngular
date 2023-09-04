a1bbd6da86b4556dca639cb9b6f6a2fa578638fc
LkrOverlord <menesestapia@gmail.com>
2023-09-03 21:36:47 -0300

    Se agrega CoreModule y SharedModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import { AuthComponent } from './components/auth/auth.component';
-import { HomeComponent } from './pages/home/home.component';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    AuthComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
+    CoreModule,
+    SharedModule,
     AppRoutingModule,
     BsDropdownModule.forRoot()
   ],
