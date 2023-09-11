10966272c16726ca3165651adcba68b667dce8cc
Willams Meneses <menesestapia@gmail.com>
2023-09-11 13:35:14 -0300

    Se modifica el share module para los componentes

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,13 +4,19 @@ import { DiscountPipe } from './pipes/discount.pipe';
 import { NavComponent } from './components/nav/nav.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { CarouselComponent } from './components/carousel/carousel.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     DiscountPipe,
     NavComponent,
     FooterComponent,
-    CarouselComponent
+    CarouselComponent,
+    LogoComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     CommonModule
